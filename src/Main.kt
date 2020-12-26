bc8076704a68433230133d426fa22a21a8769e73
test <test>
2020-12-26 22:34:43 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
-    val name = "Alex"
+fun test(args: String) {
     val a = 1
     val b = 3
-    println("Hello $name")
+    println("Hello $args")
     println(a + b)
+}
+
+fun main(args: Array<String>) {
+    val userKotlin = UserKotlin("test@test.ru")
+    println(userKotlin.email)
+    println(test("test_param"))
 }
