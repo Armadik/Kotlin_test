408f68a9c9da2ae5ba2085117aaff4a29b0d09f4
test <test>
2020-12-26 21:57:15 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,7 @@
-fun main(args: Array<String>){
-    val name: String = "Alex"
+fun main(args: Array<String>) {
+    val name = "Alex"
+    val a: Int = 1
+    val b: Int = 3
     println("Hello $name")
+    println(a + b)
 }
